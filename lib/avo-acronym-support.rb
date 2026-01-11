d5af64f61a3f7da9dc5ccf0ed6e4bcbb389f8ca4
hmk <hmk@users.noreply.github.com>
2026-01-11 15:38:08 -0800

    fix: actually load code when included

---
+++ lib/avo-acronym-support.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Entry point for avo-acronym-support gem
+# This file is automatically required by Bundler
+puts "[avo-acronym-support] Main entry point loading from #{__FILE__}"
+
+require_relative "avo/acronym/support"
+
+puts "[avo-acronym-support] Main entry point finished loading"
